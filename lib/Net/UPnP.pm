a3f2ee8ce242f1ccb1c31a5ace5f23ff31f5ff00
skonno <skonno@63374398-7d24-0410-82fc-f77bfe80c15c>
2008-09-01 09:48:29 +0000

    Added Net:UPnP:AV:MediaRender
    git-svn-id: https://clinkperl.svn.sourceforge.net/svnroot/clinkperl/trunk@13 63374398-7d24-0410-82fc-f77bfe80c15c

--- lib/Net/UPnP.pm
+++ lib/Net/UPnP.pm
@@ -9,8 +9,8 @@ use warnings;
 
 use vars qw($VERSION $DEBUG $SSDP_ADDR $SSDP_PORT);
 
-$VERSION  = '1.2.4';
-$DEBUG = 1;
+$VERSION  = '1.2.5';
+$DEBUG = 0;
 
 $SSDP_ADDR = '239.255.255.250';
 $SSDP_PORT = 1900;
